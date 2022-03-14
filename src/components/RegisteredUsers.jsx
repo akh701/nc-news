e1e31b3957aea6fc468d9dc6ab227b26682facbb
ahmed hamdi <hamdi@192.168.1.111>
2022-03-14 08:15:36 +0000

    minor updates to login css

--- src/components/RegisteredUsers.jsx
+++ src/components/RegisteredUsers.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState, useContext } from "react"
+import { useNavigate } from "react-router-dom"
 import * as api from "../api"
 import { UserContext } from "../contexts/UserContext"
 
 export default function RegisteredUsers() {
 	const { loggedInUser, setLoggedInUser } = useContext(UserContext)
 	const [allUsers, setAllUsers] = useState([])
+	let navigate = useNavigate()
 
 	useEffect(() => {
 		api.fetchUsers().then(users => {
@@ -12,12 +14,21 @@ export default function RegisteredUsers() {
 		})
 	}, [])
 
+	const handleLogOut = () => {
+		setLoggedInUser("")
+		navigate("/")
+	}
+
 	return (
 		<>
+			<span onClick={handleLogOut} className='logout'>
+				{loggedInUser.username ? "Log Out" : ""}
+			</span>
 			<ul className='dropdown login'>
 				<li className='dropbtn'>
 					{loggedInUser.username ? "Pick user" : "login"}
 				</li>
+
 				<li className='dropdown-content'>
 					<ul>
 						{allUsers.map((user, index) => {
